2435859dab4032d2754d193f6fefc3fae0e2a2ce
jghibiki <jghibiki.games@gmail.com>
2014-09-22 13:52:32 -0500

    fix song tests

--- Services/Entities/SongTests.py
+++ Services/Entities/SongTests.py
@@ -12,17 +12,17 @@ class TestSong(unittest.TestCase):
     def test_title(self):
         expected = "test_title"
         song = Song.Song.newInstance(_title=expected, _album="", _artist="")
-        self.assertTrue(song.Title == expected)
+        self.assertTrue(song.title == expected)
 
     def test_album(self):
         expected = "test_album"
         song = Song.Song.newInstance(_title="", _album=expected, _artist="")
-        self.assertTrue(song.Album == expected)
+        self.assertTrue(song.album == expected)
 
     def test_artist(self):
         expected = "test_artist"
         song = Song.Song.newInstance(_title="", _album="", _artist=expected)
-        self.assertTrue(song.Artist == expected)
+        self.assertTrue(song.artist == expected)
 
 
 if __name__ == "__main__":
