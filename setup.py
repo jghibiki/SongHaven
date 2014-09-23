f07ac4d65a9aee7224d3ecb2709bd8b116e4e685
jghibiki <jghibiki.games@gmail.com>
2014-09-23 17:07:21 -0500

    Trying a new approach with setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
 from distutils.core import setup
 
-setup()
+setup(name='SongHaven',
+        version='0.0',
+        packages=['flask', 'Jinja2', 'MarkupSafe', 'Pygments', 'Werkzueg', 'argparse', 'distribute', 'itsdangerous', 'peewee', 'wsgiref']
+        )
