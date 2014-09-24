4d432d5c1a50613499da36d1ff1186a60c9ff1bd
jghibiki <jghibiki.games@gmail.com>
2014-09-24 14:04:59 -0500

    Revert "trying to fix merge issue"
    This reverts commit 8982dc3a7012275d6a45e96e6cffa98955183aaf.

--- setup.py
+++ setup.py
@@ -4,11 +4,7 @@ from pip.req import parse_requirements
 setup(name='SongHaven',
         version='0.0',
 <<<<<<< HEAD
-<<<<<<< HEAD
         install_requires=["flask", "jinja2", "markupsafe", "pygments", "werkzeug", "argparse", "distribute", "docutils", "itsdangerous", "peewee", "wsgiref"]
-=======
-        install_requires=['flask', 'Jinja2', 'MarkupSafe', 'Pygments', 'Werkzueg', 'argparse', 'distribute', 'itsdangerous', 'peewee', 'wsgiref']
->>>>>>> parent of a56b1c0... fix requirement mispelling
 =======
         install_requires=['flask', 'Jinja2', 'MarkupSafe', 'Pygments', 'Werkzueg', 'argparse', 'distribute', 'itsdangerous', 'peewee', 'wsgiref']
 >>>>>>> parent of a56b1c0... fix requirement mispelling
