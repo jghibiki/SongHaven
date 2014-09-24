ba984f78a751623010d674935835f152057b22cf
jghibiki <jghibiki.games@gmail.com>
2014-09-24 14:31:34 -0500

    fix failed merge

--- setup.py
+++ setup.py
@@ -3,9 +3,5 @@ from pip.req import parse_requirements
 
 setup(name='SongHaven',
         version='0.0',
-<<<<<<< HEAD
-        install_requires=["flask", "jinja2", "markupsafe", "pygments", "werkzeug", "argparse", "distribute", "docutils", "itsdangerous", "peewee", "wsgiref"]
-=======
         install_requires=['flask', 'Jinja2', 'MarkupSafe', 'Pygments', 'Werkzueg', 'argparse', 'distribute', 'itsdangerous', 'peewee', 'wsgiref']
->>>>>>> parent of a56b1c0... fix requirement mispelling
         )
