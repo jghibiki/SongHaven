52a8b84a9195cd415a7e77417da857c6a3b1125f
furphy <dropshipmk4@aim.com>
2014-11-23 12:56:02 -0600

    Controllers and views for Messages, new database table, changes to home/index

--- SongHaven/SongHaven/User.cs
+++ SongHaven/SongHaven/User.cs
@@ -14,6 +14,11 @@ namespace SongHaven
     
     public partial class User
     {
+        public User()
+        {
+            this.Messages = new HashSet<Message>();
+        }
+    
         public System.Guid guid_id { get; set; }
         public string nvc_username { get; set; }
         public string nvc_password { get; set; }
@@ -23,5 +28,7 @@ namespace SongHaven
         public System.DateTime dt_created_date { get; set; }
         public int int_account_strikes { get; set; }
         public Nullable<System.DateTime> dt_date_banned { get; set; }
+    
+        public virtual ICollection<Message> Messages { get; set; }
     }
 }
