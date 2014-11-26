0dabceddb133f617561b763fceb2a057177e65ac
jghibiki <jghibiki.games@gmail.com>
2014-11-25 19:12:13 -0600

    Merge HomeController tests, eric had a better solition than mine

--- SongHaven/SongHaven/Controllers/HomeController.cs
+++ SongHaven/SongHaven/Controllers/HomeController.cs
@@ -17,18 +17,10 @@ namespace SongHaven.Controllers
         public ActionResult Index()
         {
             
-             string title = (from s in db.Requests
-                                  select s.Song.nvc_title).FirstOrDefault();
-            if (string.IsNullOrEmpty(title))
-            {
-                ViewBag.NowPlaying = "Nothing is playing...";
-            }
-            else
-            {
-                ViewBag.NowPlaying = title;
-            }
-             
-          
+            ViewBag.NowPlaying = (from s in db.Requests
+                                  select s.Song.nvc_title).FirstOrDefault().ToString();
+            if (ViewBag.NowPlaying == null)
+                ViewBag.NowPlaying = "No Song Playing";
 
             ViewBag.Requests = (from r in db.Requests
                                 select r);
@@ -40,12 +32,11 @@ namespace SongHaven.Controllers
         }
 
         [HttpPost]
-        public ActionResult Index(FormCollection form)
+        public ActionResult Index(FormCollection form, string newContent)
         {
-            var a = form[0];
+            var a = form["newContent"];
             return View();
         }
-        [HttpPost]
         public ActionResult Request(System.Guid id)
         {
 
@@ -60,11 +51,34 @@ namespace SongHaven.Controllers
 
             return View();
         }
-        public ActionResult About()
+        public ActionResult Request(Guid? id)
         {
-            ViewBag.Message = "Your application description page.";
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Song song = db.Songs.Find(id);
+            if (song == null)
+            {
+                return HttpNotFound();
+            }
 
-            return View();
+            if ((from r in db.Requests
+                 where r.fk_song == (Guid)id
+                 select r).FirstOrDefault() != null)
+            {
+                Request newRequest = new Request()
+                {
+                    guid_id = Guid.NewGuid(),
+                    dt_created_date = DateTime.Now,
+                    fk_song = (Guid)id,
+                    fk_user = Guid.Parse("B0084E4D-A166-43F6-8B1B-65387C38F5D7")
+                };
+                db.Requests.Add(newRequest);
+
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
         }
 
         public ActionResult Contact()
